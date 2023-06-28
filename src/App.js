d8e369bb9f8a6f2588a6264f8cc769c5f6099d12
Geethika S <geethu.s1995@gmail.com>
2023-06-28 19:17:26 +0530

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import LoginForm from './Components/loginForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import CustomerLogin from './Components/customerLoginPage';
 import AdminLogin from './Components/adminLoginPage';
 
 function App() {
   return (
+    <>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">JUHOSI</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+ 
     <BrowserRouter>
       <Routes>
         <Route >
@@ -17,6 +34,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
